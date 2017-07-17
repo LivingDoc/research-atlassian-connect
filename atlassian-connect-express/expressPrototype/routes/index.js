cbd62610a747a1ae04ee60bd822838ef5c998732
Nico Paul <nicopaul49@googlemail.com>
2017-07-17 10:08:28 +0200

    Express: Refactored static macros, general pages and atlassian connect.json

--- atlassian-connect-express/expressPrototype/routes/index.js
+++ atlassian-connect-express/expressPrototype/routes/index.js
@@ -18,10 +18,10 @@ module.exports = function (app, addon) {
 
     // This is an example route that's used by the default "generalPage" module.
     // Verify that the incoming request is authenticated with Atlassian Connect
-    app.get('/hello-world', addon.authenticate(), function (req, res) {
+    app.get('/express/generalPage/hello-world', addon.authenticate(), function (req, res) {
             // Rendering a template is easy; the `render()` method takes two params: name of template
             // and a json object to pass the context in
-            res.render('hello-world', {
+            res.render('general-page-hello-world', {
                 title: 'Atlassian Connect'
                 //issueId: req.query['issueId']
             });
@@ -29,17 +29,20 @@ module.exports = function (app, addon) {
     );
 
     // Add any additional route handlers you need for views or REST resources here...
-    app.get('/test-page', addon.authenticate(), function(req, res) {
-        res.render('test-page', {
+    app.get('/express/generalPage/test-page', addon.authenticate(), function(req, res) {
+        res.render('general-page-test-page', {
             title: 'This is a title!'
         });
     });
 
-    app.get('/macro/static/hello-world-macro', addon.authenticate(), function(req, res) {
+
+    // STATIC CONTENT MACROS
+
+    app.get('/express/macro/static/hello-world', addon.authenticate(), function(req, res) {
       let thisWorld = req.query.thisWorld
       let personName = req.query.personName
       let pageTitle = req.query.pageTitle
-      res.render('static-hello-world-macro', {
+      res.render('static-macro-hello-world', {
         thisWorld: thisWorld
         , personName: personName
         , pageTitle: pageTitle
@@ -47,16 +50,16 @@ module.exports = function (app, addon) {
     });
 
 
-    app.get('/macro/static/time-macro', addon. authenticate(), function(req, res) {
+    app.get('/express/macro/static/date-time', addon.authenticate(), function(req, res) {
       let currentDateTime = new Date()
       let date = currentDateTime.getDate() + "." + (currentDateTime.getMonth()+1) + "." + currentDateTime.getFullYear()
       let time = currentDateTime.getHours() + ":" + currentDateTime.getMinutes() + ":" + currentDateTime.getSeconds()
-      res.render('static-time-macro', {
+      res.render('static-macro-date-time', {
         date: date
         , time: time
       })
     })
-    
+
     // load any additional files you have in routes and apply those to the app
     {
         var fs = require('fs');
