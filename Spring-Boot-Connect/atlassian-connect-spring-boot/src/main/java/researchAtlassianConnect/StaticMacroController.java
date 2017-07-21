88908d2a5969f008e88abf84b621f242f0a3c34a
Nico Paul <nicopaul49@googlemail.com>
2017-07-21 15:06:59 +0200

    Spring: Added StaticHelloWorldMacro with parameters and dependency to confluence page Title

--- Spring-Boot-Connect/atlassian-connect-spring-boot/src/main/java/researchAtlassianConnect/StaticMacroController.java
+++ Spring-Boot-Connect/atlassian-connect-spring-boot/src/main/java/researchAtlassianConnect/StaticMacroController.java
@@ -4,6 +4,8 @@ package researchAtlassianConnect;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -15,6 +17,18 @@ public class StaticMacroController {
     public String DateTimeMacro(Model model) {
         addDateAndTime(model);
         return "staticDateTimeMacro";
+    }
+
+    @RequestMapping(value="/macro/static/hello-world-macro", params = {"thisWorld", "personName", "pageTitle"})
+    public String helloWorldMacro(Model model
+        , @RequestParam(value = "thisWorld") String thisWorld
+        , @RequestParam(value = "personName") String personName
+        , @RequestParam(value = "pageTitle") String pageTitle)
+    {
+        model.addAttribute("thisWorld", thisWorld);
+        model.addAttribute("personName", personName);
+        model.addAttribute("pageTitle", pageTitle);
+        return "staticHelloWorldMacro";
     }
 
     private void addDateAndTime(Model model) {
