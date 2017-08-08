c9670bff9f6f738389fe5ce83eeccf30432d9a33
Nico Paul <nicopaul49@googlemail.com>
2017-08-08 13:01:30 +0200

    Spring: Added dynamic macro to convert jpeg image to base64 String and to display the locally stored image on confluence page

--- Spring-Boot-Connect/atlassian-connect-spring-boot/src/main/java/researchAtlassianConnect/DynamicMacroController.java
+++ Spring-Boot-Connect/atlassian-connect-spring-boot/src/main/java/researchAtlassianConnect/DynamicMacroController.java
@@ -1,7 +1,13 @@
 package researchAtlassianConnect;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Base64;
 
 @Controller
 public class DynamicMacroController {
@@ -12,4 +18,24 @@ public class DynamicMacroController {
         return "macro/dynamic/dateTime";
     }
 
+
+    // Macro which converts a local stored image to base64 and displays it on the returned html file
+    @GetMapping("/spring/macro/dynamic/image-to-base64")
+    public String imageToBase64(Model model) {
+
+        File file = new File("C:/Workspace/src/NT/research-atlassian-connect/Spring-Boot-Connect/atlassian-connect-spring-boot/src/main/resources/falloutVaultBoyThumbsUp.jpg");
+
+        String base64String = null;
+        try {
+            FileInputStream fileInputStreamReader = new FileInputStream(file);
+            byte[] bytes = new byte[(int) file.length()];
+            fileInputStreamReader.read(bytes);
+            base64String = Base64.getEncoder().encodeToString(bytes);
+            model.addAttribute("base64String", base64String);
+        } catch (IOException e) {
+
+        }
+
+        return "macro/dynamic/imageToBase64";
+    }
 }
